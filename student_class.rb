b70d84b799fde998f12330114a36a840b2ef5b12
ibugithub <outsideworkibrahim@gmail.com>
2023-08-03 15:49:30 +0600

    Update associations

--- student_class.rb
+++ student_class.rb
@@ -1,10 +1,17 @@
 require './person_class'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown')
     super(age, name)
     @classroom = classroom
     classroom.add_student(self)
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
