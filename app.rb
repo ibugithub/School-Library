3bffd05ca01c529799487d1775b1a6b7bf71c467
ibugithub <outsideworkibrahim@gmail.com>
2023-08-05 00:36:39 +0600

    Created main and app file

--- app.rb
+++ app.rb
@@ -10,6 +10,16 @@ class App
     @rentals = []
   end
 
+  def intro
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+
   def show_people(show_index: false)
     if @people.empty?
       puts 'There are no people available'
